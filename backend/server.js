dd4f5eace56aafa82f69398d82c7f936cfdcfcc4
devatgoodlives <83652199+devatgoodlives@users.noreply.github.com>
2021-10-12 13:22:31 +0530

    added mongodb models

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
-const express = require("express")
-const products = require("./data/products")
+import express from "express"
+import dotenv from "dotenv"
+import colors from "colors"
+import connectDB from "./config/db.js"
+import products from "./data/products.js"
+
 const app = express()
 
+dotenv.config()
+
+connectDB()
+
 app.get("/", (req, res) => {
-  res.send("API is running...")
+  res.send("API is running....")
 })
 
 app.get("/api/products", (req, res) => {
@@ -14,4 +22,12 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
-app.listen(5000, console.log("Server running on port 5000"))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  5000,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
