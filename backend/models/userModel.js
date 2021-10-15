436a7d252ba7a49fd40a579e88eef3085f33a8a5
devatgoodlives <83652199+devatgoodlives@users.noreply.github.com>
2021-10-15 17:21:36 +0530

    implemented authentication in backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import bcyrpt from "bcryptjs"
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,18 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 )
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcyrpt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next()
+  }
+  const salt = await bcyrpt.genSalt(10)
+  this.password = await bcyrpt.hash(this.password, salt)
+})
 
 const User = mongoose.model("User", userSchema)
 
