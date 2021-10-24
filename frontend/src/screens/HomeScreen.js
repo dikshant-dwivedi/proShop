94125e4d129c7ab18dceb73f4f628206cd27803a
devatgoodlives <83652199+devatgoodlives@users.noreply.github.com>
2021-10-24 12:05:05 +0530

    paginated products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,17 +5,18 @@ import { Row, Col } from "react-bootstrap"
 import { listProducts } from "../actions/productActions"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
+import Paginate from "../components/Paginate"
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword
   const dispatch = useDispatch()
-
+  const pageNumber = match.params.pageNumber || 1
   const productList = useSelector((state) => state.productList)
-  const { loading, error, products } = productList
+  const { loading, error, products, page, pages } = productList
 
   useEffect(() => {
-    dispatch(listProducts(keyword))
-  }, [dispatch, keyword])
+    dispatch(listProducts(keyword, pageNumber))
+  }, [dispatch, keyword, pageNumber])
 
   return (
     <div>
@@ -29,13 +30,22 @@ const HomeScreen = ({ match }) => {
           <Message variant='danger'>{error}</Message>
         </h3>
       ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xlg={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xlg={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Row>
+            <Paginate
+              pages={pages}
+              page={page}
+              keyword={keyword ? keyword : ""}
+            />
+          </Row>
+        </>
       )}
     </div>
   )
